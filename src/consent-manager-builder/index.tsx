22fee415b7c2177701ba27737e9472a40a483b3b
Adrian Manjarres <adrian.manjarres@avant.com>
2024-11-12 10:27:44 -0500

    hotfix

--- src/consent-manager-builder/index.tsx
+++ src/consent-manager-builder/index.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import { deleteCookiesOnPreferencesChange, loadPreferences, savePreferences } from './preferences
+import { deleteCookiesOnPreferencesChange, loadPreferences, savePreferences } from './preferences'
 
 import {
   Destination,
